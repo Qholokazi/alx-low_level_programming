898100639e64e23ef10714878cbf4d9d8421c665
Zizile Mdutshane <Sisonkezeemdu@gmail.com>
2023-06-22 08:23:25 -0700

    Created gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-mul.c -o 2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Entry point
+ * @a: - The first integer
+ * @b: - The second integer
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+int result = a * b;
+return (result);
+}
