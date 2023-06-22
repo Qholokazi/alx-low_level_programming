2b3e7697c5109df3d8da66297d140e59376932a7
Zizile Mdutshane <Sisonkezeemdu@gmail.com>
2023-06-22 10:25:38 -0700

    Created 10-triangles.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -18,6 +18,7 @@ void print_triangle(int size)
 	else
 	{
 		for (row = 1; row <= size; row++)
+		{
 			for (spaces = size - row; spaces >= 1; spaces--)
 			{
 				_putchar(' ');
@@ -27,5 +28,6 @@ void print_triangle(int size)
 			_putchar('#');
 		}
 		_putchar('\n');
+	}
 	}
 }
