48101f2008749d80901c6781cc9677780b5a1b4b
Zizile Mdutshane <Sisonkezeemdu@gmail.com>
2023-06-28 03:10:56 -0700

    Created 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - Entry point
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	while (src[a] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+
+	dest[a] = '\0';
+	return (dest);
+}
