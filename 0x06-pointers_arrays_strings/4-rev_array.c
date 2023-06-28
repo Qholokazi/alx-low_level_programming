267d9e0e41267250d73b5b6a9e003003801f6844
Zizile Mdutshane <Sisonkezeemdu@gmail.com>
2023-06-28 03:48:07 -0700

    Created 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - Entry point
+ * @a: array
+ * @n: number of elements of array
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int d;
+	int e;
+
+	for (d = 0; d < n--; d++)
+	{
+		e = a[d];
+		a[d] = a[n];
+		a[n] = e;
+	}
+}
