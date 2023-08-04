5c94f84177367e93825c171d26093fffab185241
Zizile Mdutshane <Sisonkezeemdu@gmail.com>
2023-08-04 05:06:13 -0700

    Created listuint_len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - stsarting point
+ * @h: linked list of type listint_t to traverse
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t b = 0;
+
+	while (h)
+	{
+		b++;
+		h = h->next;
+	}
+	return (b);
+}
